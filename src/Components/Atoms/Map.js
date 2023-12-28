f19e893221db43971dec1b4f74fb1cd4253b38ab
Sinwan Meynaud <sinwan.meynaud@gmail.com>
2023-12-28 18:38:23 +0100

    add standaloneearchbox in maps and remove protected logic to all route

--- src/Components/Atoms/Map.js
+++ src/Components/Atoms/Map.js
@@ -1,5 +1,26 @@
 import React from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  StandaloneSearchBox,
+} from "@react-google-maps/api";
+
+const inputStyle = {
+  boxSizing: `border-box`,
+  border: `1px solid transparent`,
+  width: `240px`,
+  height: `32px`,
+  padding: `0 12px`,
+  borderRadius: `3px`,
+  boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+  fontSize: `14px`,
+  outline: `none`,
+  textOverflow: `ellipses`,
+  position: "absolute",
+  top: "10px",
+  right: "10px",
+};
 
 export default function Map() {
   const libraries = ["places"];
@@ -31,6 +52,13 @@ export default function Map() {
         zoom={10}
         center={center}
       >
+        <StandaloneSearchBox>
+          <input
+            type="text"
+            placeholder="Customized your placeholder"
+            style={inputStyle}
+          />
+        </StandaloneSearchBox>
         <Marker position={center} />
       </GoogleMap>
     </div>
--- src/Components/Atoms/Protected.js
+++ src/Components/Atoms/Protected.js
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 
 function Protected({ isLoggedIn, children }) {
   console.log(isLoggedIn);
-  if (isLoggedIn.token === "undefined") {
+  if (!isLoggedIn) {
     return <Navigate to="/" replace />;
   }
   return children;
